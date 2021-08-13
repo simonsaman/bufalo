c31025643dbca4b9b4c76bf0d3faf4b2c6b98e58
Simón Samán <simonsaman@gmail.com>
2021-08-13 04:45:48 -0400

    Optimizaciones y paralelismo

--- prueba.py
+++ prueba.py
@@ -1,10 +1,13 @@
 # Prueba de Simón Samán (©2021)
 from copy import deepcopy
+import multiprocessing as mp
+
 path = 'example.txt'
 bombillo = "💡"
 piso = "⬜"
 pared = "⬛"
 datos = []
+barra = "____________________________________"
 minBombillos = -1
 mejorCaso = 0
 datosCargados = False
@@ -50,7 +53,7 @@ def puntuacionCelda(matriz, i,j):
       continue
     if matriz[y][j] == bombillo:
       return 0
-    if matriz[y][j] <= 1 :
+    if matriz[y][j] == 0 :
       continue
     puntos+= 1
 
@@ -63,7 +66,7 @@ def puntuacionCelda(matriz, i,j):
     if matriz[i][x] == piso:
       puntos+= 1
       continue
-    if matriz[i][x] <= 1 :
+    if matriz[i][x] == 0 :
       continue
     puntos+= 1
   
@@ -76,7 +79,7 @@ def puntuacionCelda(matriz, i,j):
     if matriz[y][j] == piso:
       puntos += 1
       continue
-    if matriz[y][j] <= 1 :
+    if matriz[y][j] == 0 :
       continue
     puntos += 1
   
@@ -89,7 +92,7 @@ def puntuacionCelda(matriz, i,j):
     if matriz[i][x] == piso:
       puntos += 1
       continue
-    if matriz[i][x] <= 1:
+    if matriz[i][x] == 0:
       continue
     puntos += 1
   
@@ -115,61 +118,103 @@ def generarPrioridad(matriz):
       if matriz[i][j] == bombillo or matriz[i][j] == pared or matriz[i][j] <= 0 :
         continue
       prioridad.append((matriz[i][j],i,j))
-
   prioridad.sort(reverse=True,key=lambda x: x[0])
   return prioridad
 
+def siguienteObjetivo(matriz):
+  max = 0
+  siguienteI = -1
+  siguienteJ = -1
+  for i in range(len(matriz)):
+    for j in range(len(matriz[0])):
+      if matriz[i][j] == bombillo or matriz[i][j] == pared or matriz[i][j] <= 0 :
+        continue
+      if matriz[i][j] > max:
+        max = matriz[i][j]
+        siguienteI = i
+        siguienteJ = j
+  return (max, siguienteI, siguienteJ)
+
 #Búsqueda usando DFS
-def buscarSolucion(i,j,conteoBombillos, datos):
-  global minBombillos, mejorCaso
+def buscarSolucion(i, j, conteoBombillos, datos, listaResultados, minBombillos):
   # Podando soluciones inviables
   if (minBombillos != -1 and conteoBombillos >= minBombillos):
-    return
+    return -1
   datos[i][j] = bombillo
   puntuaciones = determinarPuntuaciones(datos)
-  listaPrioridad = generarPrioridad(puntuaciones)
+  siguientePrioridad = siguienteObjetivo(puntuaciones)
+  (valor, x, y) = siguientePrioridad
   #Condicion de parada
-  if len(listaPrioridad) == 0:
+  if valor == 0:
     # Nuevo minimo
-    if (conteoBombillos < minBombillos or minBombillos == -1):
-      minBombillos = conteoBombillos
-      mejorCaso = deepcopy(puntuaciones)
-    return
+    if (conteoBombillos < minBombillos or minBombillos == -1): 
+      listaResultados.append((conteoBombillos, deepcopy(puntuaciones)))
   else:
     # Paso en profundidad
-    (valor, k, l) = listaPrioridad[0]
-    buscarSolucion(k, l, conteoBombillos + 1, datos)
-    return
+    return buscarSolucion(x, y, conteoBombillos + 1, puntuaciones, listaResultados, minBombillos)
+
+def lote(lista, paquete):
+  largo = len(lista)
+  for i in range(0, largo, paquete):
+    yield lista[i:min(i + paquete, largo )]
+
 
 def menu():
-  print ("____________________________________")
+  print (barra)
   if datosCargados:
-    print("(3) Mostar solución")
+    print("(3) Mostrar solución")
     print("(2) Mostrar habitación")
   else:
     print("(1) Cargar datos desde example.txt ")
   print("(0) Salir")
-  print ("____________________________________")
+  print (barra)
 
-menu()
-opcionMenu = input("")
+def main():
+  global datosCargados, minBombillos, mejorCaso,opcionMenu
+  procesadores =  mp.cpu_count()
 
-while opcionMenu != "0":
-  if opcionMenu == "1":
-    cargarDatos()
-    datosCargados = True
-  elif opcionMenu == "2":
-    imprimirMatrizEmoji(datos)
-  elif opcionMenu == "3":
-    for y in range(len(datos)):
-      for x in range(len(datos[y])):
-        if datos[y][x]  == pared:
-          continue
-        nuevosDatos = deepcopy(datos)
-        buscarSolucion(y,x,1, nuevosDatos)
-    imprimirMatrizEmoji(mejorCaso)
-    print("Numero mínimo de bombillos: ",minBombillos)
-  else:
-    print("Opción no reconocida")
   menu()
   opcionMenu = input("")
+
+  while opcionMenu != "0":
+    if opcionMenu == "1":
+      cargarDatos()
+      datosCargados = True
+    elif opcionMenu == "2":
+      imprimirMatrizEmoji(datos)
+    elif opcionMenu == "3":
+    # Ciclo de búsqueda
+      puntuaciones = determinarPuntuaciones(datos)
+      listaPrioridad = generarPrioridad(puntuaciones)
+      # Encolar en baches para cada procesador
+      for bache in lote(listaPrioridad, procesadores):
+        manager = mp.Manager()
+        listaResultados = manager.list()
+        procesos = []
+        for candidato in bache:
+          (_ ,i,j) = candidato 
+          if candidato == pared:
+              continue
+          nuevosDatos = deepcopy(datos)
+          proceso = mp.Process(target = buscarSolucion, args=[i, j, 1, nuevosDatos, listaResultados, minBombillos])
+          procesos.append(proceso)
+          proceso.start()
+          
+        for proceso in procesos:
+          proceso.join()
+
+        for resultado in listaResultados:
+            (valor, solucion) = resultado
+            if minBombillos == -1 or  valor < minBombillos:
+              minBombillos = valor
+              mejorCaso = deepcopy(solucion)
+      imprimirMatrizEmoji(mejorCaso)
+      print("Numero mínimo de bombillos: ", minBombillos)
+    else:
+      print("Opción no reconocida")
+    menu()
+    opcionMenu = input("")
+    print (barra)
+
+if __name__ == "__main__":
+  main()
